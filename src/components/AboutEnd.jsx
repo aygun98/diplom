93ce2a728b3d251314f3e531c00331f4d6351e1b
Aygun <aygun.xuduyeva98@gmail.com>
2023-04-03 08:28:25 -0700

    about end bitdi

--- src/components/AboutEnd.jsx
+++ src/components/AboutEnd.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './css/AboutEnd.css'
+import {RiNumber9} from 'react-icons/ri'
 
 const AboutEnd = () => {
   return (
-    <div>AboutEnd</div>
+    <div className='about-end'>
+        <div className='about-end-top'></div>
+        <div div className='about-end-bottom-container'>
+            <div className="about-end-bottom">
+            <div className='about-end-bottom-icons'>
+            <RiNumber9 className='about-end-bottom-nomre-icon-margin '/>
+             <RiNumber9 className='about-end-bottom-nomre-icon'/>
+            </div>
+            <h1>Günün sonunda içməyin asan olduğunu düşünmürəm. Pulsuz iki protein çubuğu.</h1>
+           <h6>James Arthur</h6>
+        </div>
+        </div>
+    </div>
   )
 }
 
